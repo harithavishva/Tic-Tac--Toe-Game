643f8875c308a6d9776d3486d21ce8d565411fcf
harithavishva <harithasai01@gmail.com>
2023-04-02 11:40:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Confite from "react-confetti";
 
-function App() {
+export default function App() {
+  const [isXtrun, setIsXturn] = useState(true);
+  const [board, setBoard] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
+  ]);
+
+  const winnerLogic = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [0, 3, 6],
+      [0, 4, 8],
+      [1, 4, 7],
+      [2, 4, 6],
+      [2, 5, 8],
+      [3, 4, 5],
+      [6, 7, 8]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      //[0, 1, 2]
+      // no value should be null
+      // a is equal to b 0
+      // b is equal to c 0
+
+      if (board[a] != null && board[a] === board[b] && board[b] === board[c]) {
+        console.log("winner is ", board[a]);
+        return board[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = winnerLogic(board);
+
+  const handleClick = (idx) => {
+    // check box is empty or null
+    if (!board[idx]) {
+      // copy of an array
+      console.log("board : ", board);
+      // new array set
+      const boardCopy = [...board];
+      boardCopy[idx] = isXtrun ? "X" : "O";
+      console.log("BoardCopy :", boardCopy);
+      // oldarray - new array
+      setBoard(boardCopy);
+      setIsXturn(!isXtrun);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {winner ? <Confite /> : ""}
+      <div className="App">
+        {board.map((val, index) => (
+          <GameBox
+            key={index}
+            val={val}
+            onPlayersClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+      <br />
+      <div>
+        {winner ? <h1>Winner is {winner}</h1> : ""}
+        <button
+          onClick={() => {
+            setBoard([null, null, null, null, null, null, null, null, null]);
+            setIsXturn(true);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Restart
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+function GameBox({ val, onPlayersClick }) {
+  return (
+    <div
+      style={{ color: val === "X" ? "crimson" : "green" }}
+      onClick={() => onPlayersClick()}
+      className="game-box"
+    >
+      {val}
+    </div>
+  );
+}
